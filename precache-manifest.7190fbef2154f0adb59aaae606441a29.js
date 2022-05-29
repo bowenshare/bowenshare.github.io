8b2a16f147701c1d7369e87f7ed82e5ea5d8d070
redhat163 <25673825+redhat163@users.noreply.github.com>
2022-05-29 08:50:41 -0700

    add build scripts

---
+++ precache-manifest.7190fbef2154f0adb59aaae606441a29.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38b2e147ed39263758b4135b8043568e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c279f9db131615b1d3b1",
+    "url": "/static/css/2.c1f96cec.chunk.css"
+  },
+  {
+    "revision": "f655553a36836fdc5257",
+    "url": "/static/css/main.882e49e6.chunk.css"
+  },
+  {
+    "revision": "c279f9db131615b1d3b1",
+    "url": "/static/js/2.cceb53a6.chunk.js"
+  },
+  {
+    "revision": "d3e49d62c5e83bd760c7639e00b15fb0",
+    "url": "/static/js/2.cceb53a6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74a6d00f13236e09d794",
+    "url": "/static/js/3.7d16ccaa.chunk.js"
+  },
+  {
+    "revision": "f655553a36836fdc5257",
+    "url": "/static/js/main.c27439cf.chunk.js"
+  },
+  {
+    "revision": "d7b9242d08da1e1229c2",
+    "url": "/static/js/runtime~main.f4a5e6fa.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
