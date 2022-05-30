243d71e0cbc1a6a117f8e29081585058e124d75a
redhat163 <25673825+redhat163@users.noreply.github.com>
2022-05-29 17:17:10 -0700

    add

--- src/elements/Math.jsx
+++ src/elements/Math.jsx
@@ -7,36 +7,44 @@ import Header from "../component/header/Header";
 import Footer from "../component/footer/Footer";
 
 
-const ServiceList = [
+const MathCompetitionList = [
     {
         icon: <FiCast />,
-        title: 'Business Stratagy',
+        title: 'Math Kangaroo',
         description: 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut.'
     },
+    // {
+    //     icon: <FiLayers />,
+    //     title: 'Website Development',
+    //     description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
+    // },
+    // {
+    //     icon: <FiUsers />,
+    //     title: 'Marketing & Reporting',
+    //     description: 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut.'
+    // },
+    // {
+    //     icon: <FiMonitor />,
+    //     title: 'Mobile Development',
+    //     description: 'Desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    // },
+    // {
+    //     icon: <FiMail />,
+    //     title: 'Email Marketing',
+    //     description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in.'
+    // },
+    // {
+    //     icon: <FiCopy />,
+    //     title: 'Copywriting',
+    //     description: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for.'
+    // },
+]
+
+const MathLearningList = [
     {
-        icon: <FiLayers />,
-        title: 'Website Development',
-        description: 'I throw myself down among the tall grass by the stream as I lie close to the earth.'
-    },
-    {
-        icon: <FiUsers />,
-        title: 'Marketing & Reporting',
+        icon: <FiCast />,
+        title: 'Beast Acedemy',
         description: 'Lorem ipsum dolor sit amet, adipiscing elit, sed do eiusmod tempor incididunt ut.'
-    },
-    {
-        icon: <FiMonitor />,
-        title: 'Mobile Development',
-        description: 'Desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-    },
-    {
-        icon: <FiMail />,
-        title: 'Email Marketing',
-        description: 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in.'
-    },
-    {
-        icon: <FiCopy />,
-        title: 'Copywriting',
-        description: 'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for.'
     },
 ]
 
@@ -45,11 +53,11 @@ class Service extends Component{
     render(){
         return(
             <React.Fragment>
-                <PageHelmet pageTitle='Service' />
+                <PageHelmet pageTitle='Math' />
                 <Header headertransparent="header--transparent" colorblack="color--black" logoname="logo.png" />
 
                 {/* Start Breadcrump Area */}
-                <Breadcrumb title={'Service'}   />
+                <Breadcrumb title={'Math'}   />
                 {/* End Breadcrump Area */}
 
                 {/* Start Service Area */}
@@ -58,16 +66,16 @@ class Service extends Component{
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="section-title text-center mb--30">
-                                    <span className="subtitle">Our Marketing</span>
-                                    <h2>Digital Marketing</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, <br /> but the majority have suffered alteration.</p>
+                                    <span className="subtitle">My Math</span>
+                                    <h2>Math Competitions</h2>
+                                    <p>I love math and enjoy all kind of math activties.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="row service-main-wrapper">
-                            {ServiceList.map( (val , i) => (
+                            {MathCompetitionList.map( (val , i) => (
                                 <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-                                    <a href="/service-details">
+                                    <a href="/math-details">
                                         <div className="service service__style--2 text-left">
                                             <div className="icon">
                                                 {val.icon}
@@ -91,83 +99,16 @@ class Service extends Component{
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="section-title text-center mb--30">
-                                    <span className="subtitle">Our Strategy</span>
-                                    <h2>Business Strategy</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, <br /> but the majority have suffered alteration.</p>
+                                    <span className="subtitle">My Math</span>
+                                    <h2>Math Learning</h2>
+                                    <p>I love math and enjoy all kind of math activties.</p>
                                 </div>
                             </div>
                         </div>
                         <div className="row service-main-wrapper">
-                            {ServiceList.map( (val , i) => (
+                            {MathLearningList.map( (val , i) => (
                                 <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-                                    <a className="text-center" href="/service-details">
-                                        <div className="service service__style--2">
-                                            <div className="icon">
-                                                {val.icon}
-                                            </div>
-                                            <div className="content">
-                                                <h3 className="title">{val.title}</h3>
-                                                <p>{val.description}</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                {/* End Service Area */}
-
-                {/* Start Service Area */}
-                <div className="service-area ptb--120 bg_color--5">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="section-title text-center mb--30">
-                                    <span className="subtitle">Our Creatity</span>
-                                    <h2>Creative Agency</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, <br /> but the majority have suffered alteration.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="row service-main-wrapper">
-                            {ServiceList.map( (val , i) => (
-                                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-                                    <a href="/service-details">
-                                        <div className="service service__style--2 text-left bg-gray">
-                                            <div className="icon">
-                                                {val.icon}
-                                            </div>
-                                            <div className="content">
-                                                <h3 className="title">{val.title}</h3>
-                                                <p>{val.description}</p>
-                                            </div>
-                                        </div>
-                                    </a>
-                                </div>
-                            ))}
-                        </div>
-                    </div>
-                </div>
-                {/* End Service Area */}
-
-                {/* Start Service Area */}
-                <div className="service-area ptb--120 bg_color--1">
-                    <div className="container">
-                        <div className="row">
-                            <div className="col-lg-12">
-                                <div className="section-title text-center mb--30">
-                                    <span className="subtitle">Our Development</span>
-                                    <h2>Business Development</h2>
-                                    <p>There are many variations of passages of Lorem Ipsum available, <br /> but the majority have suffered alteration.</p>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className="row service-main-wrapper">
-                            {ServiceList.map( (val , i) => (
-                                <div className="col-xl-4 col-lg-4 col-md-6 col-sm-6 col-12" key={i}>
-                                    <a className="text-center" href="/service-details">
+                                    <a className="text-center" href="/service-math">
                                         <div className="service service__style--2">
                                             <div className="icon">
                                                 {val.icon}
