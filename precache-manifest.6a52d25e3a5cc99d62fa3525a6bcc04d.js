aab22d0027b67440e8a88785d6279abcce795974
redhat163 <25673825+redhat163@users.noreply.github.com>
2022-05-30 15:00:22 -0700

    add

---
+++ precache-manifest.6a52d25e3a5cc99d62fa3525a6bcc04d.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6749c4f1e458ca3a48c6b0b379576447",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e0b5ba6d9fb3a0fd8f8",
+    "url": "/static/css/2.30cd6ef5.chunk.css"
+  },
+  {
+    "revision": "da57aa64b4a35b3356ed",
+    "url": "/static/css/main.882e49e6.chunk.css"
+  },
+  {
+    "revision": "7e0b5ba6d9fb3a0fd8f8",
+    "url": "/static/js/2.b2a8760d.chunk.js"
+  },
+  {
+    "revision": "9d443d85da981ad9a2f4f49fbb5c8300",
+    "url": "/static/js/2.b2a8760d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da57aa64b4a35b3356ed",
+    "url": "/static/js/main.30c26eac.chunk.js"
+  },
+  {
+    "revision": "c37a5ee2d1983ad905dd",
+    "url": "/static/js/runtime~main.d85120f4.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02f2d4681cc07442149cd3d0061b94e4.js"
+  "/precache-manifest.6a52d25e3a5cc99d62fa3525a6bcc04d.js"
 );
 
 self.addEventListener('message', (event) => {
